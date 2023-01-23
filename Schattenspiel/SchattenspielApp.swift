fd9d27aa7ea5ef2eca63b3e97f2347ff49c08528
Enie Weiß <weiss.enie@gmail.com>
2023-01-23 15:06:32 +0100

    Initial Commit

---
+++ Schattenspiel/SchattenspielApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchattenspielApp.swift
+//  Schattenspiel
+//
+//  Created by Enie Weiß on 23.01.23.
+//
+
+import SwiftUI
+
+@main
+struct SchattenspielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
